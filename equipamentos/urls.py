4eac209cb34c4bd254fb9e423b0f064080c5c7ba
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-08-29 16:41:16 -0300

    fix: equipamento detail class import into equipamentos/urls.py

--- equipamentos/urls.py
+++ equipamentos/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import EquipamentoListCreate, EquipamentoDetail
+from .views import EquipamentoListCreate, EquipamentoDetailUpdateDelete
 
 urlpatterns = [
     path('equipamentos/', EquipamentoListCreate.as_view(), name='equipamento-list-create'),
-    path('equipamentos/<int:pk>/', EquipamentoDetail.as_view(), name='equipment-detail-update-delete'),
+    path('equipamentos/<int:pk>/', EquipamentoDetailUpdateDelete.as_view(), name='equipment-detail-update-delete'),
 ]
