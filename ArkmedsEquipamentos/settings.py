a8bae21c7b598817d00ad687964625c5b3e39770
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-09-01 13:15:01 -0300

    feat: sqlite database launched + dockerfile and docker-compose

--- ArkmedsEquipamentos/settings.py
+++ ArkmedsEquipamentos/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -86,8 +86,8 @@ WSGI_APPLICATION = 'ArkmedsEquipamentos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, os.environ.get('DB_NAME', 'db.sqlite3')),
     }
 }
 
