dc0cdffca91e39f196168931541b70e8c85ed15d
Samuel Miranda <samuelmirandasamt@gmail.com>
2024-08-31 21:51:03 -0300

    build: preparing codebase to separate repositories + setting up tests

--- equipamentos/serializers.py
+++ equipamentos/serializers.py
@@ -5,3 +5,10 @@ class EquipamentoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Equipamento
         fields = '__all__'
+        extra_kwargs = {
+            'nome': {'required': True},
+            'tipo': {'required': True},
+            'fabricante': {'required': True},
+            'modelo': {'required': True},
+            'numero_serie': {'required': True},
+        }
--- equipamentos/tests.py
+++ equipamentos/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from rest_framework.test import APIClient
+from django.urls import reverse
+from .models import Equipamento
 
-# Create your tests here.
+class EquipamentoAPITest(TestCase):
+
+    def setUp(self):
+        self.client = APIClient()
+        self.equipamento_data = {
+            "nome": "Equipamento Teste",
+            "descricao": "Descrição do equipamento teste",
+            "status": "Em Estoque",
+            "tipo": "Tipo Teste",
+            "fabricante": "Fabricante Teste",
+            "modelo": "Modelo Teste",
+            "numero_serie": "1234567890",
+            "data_compra": "2024-08-31",
+            "valor_compra": "1000.00"
+        }
+        self.url = reverse('equipamento-list-create')
+
+    def test_create_equipamento_success(self):
+        response = self.client.post(self.url, self.equipamento_data, format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Equipamento.objects.count(), 1)
+
+    def test_create_equipamento_missing_fields(self):
+        invalid_data = self.equipamento_data.copy()
+        invalid_data.pop('nome')
+        response = self.client.post(self.url, invalid_data, format='json')
+        self.assertEqual(response.status_code, 400)
